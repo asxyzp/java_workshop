422d8b00dca97c9f53884d0372993cfdef6ca97d
Aashish Loknath Panigrahi <asxyzp@gmail.com>
2021-02-10 13:16:57 +0530

    Is it possible to declare a reference data type w/o initialization?

---
+++ src/chp0/ReferenceDeclaration.java
@@ -0,0 +1,12 @@
+/*
+* Is it possible to declare a reference data type w/o initialization?
+* Created by Aashish Loknath Panigrahi*/
+package chp0;
+
+public class ReferenceDeclaration {
+    public static void main(String[] args){
+        ReferenceDeclaration refDecObject;
+        String str;
+        //ANS : Yes!
+    }
+}
