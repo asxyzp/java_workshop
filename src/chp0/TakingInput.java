a7cb6ffd5156b78bcd67a8137eb4970664da9567
Aashish Loknath Panigrahi <asxyzp@gmail.com>
2021-02-10 15:03:56 +0530

    Taking input from command line using Scanner class.

---
+++ src/chp0/TakingInput.java
@@ -0,0 +1,16 @@
+/*
+* Understanding how to take command-line input in Java
+* Created by Aashish L. Panigrahi*/
+package chp0;
+import java.util.Scanner;
+public class TakingInput {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("a?\t");
+        double a = scan.nextDouble();
+        System.out.println("b?\t");
+        double b = scan.nextDouble();
+        double c= Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+        System.out.printf("%f=√%f^2+%f^2",c,a,b);
+    }
+}
